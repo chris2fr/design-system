31b06c393bb464ee61d494d2dc14bab4c1ccd1df
Chris Mann <chris@lesgtrandsvoisins.com>
2024-05-26 15:46:00 +0200

    0.0.4

--- workshop/filenamechanger.sh
+++ workshop/filenamechanger.sh
@@ -1,7 +1,7 @@
 echo "File Name Changer"
-from="fastoche"
-to="cefran"
-for i in `find . -name "*$from*"` 
+from="cefran_lesgrandsvoisins"
+to="theme_lesgrandsvoisins"
+for i in `find . -name "*$from*"  -not -path "./venv/*"` 
     do echo "mv $i ${i/$from/$to}"
     mv $i ${i/$from/$to}
 done
